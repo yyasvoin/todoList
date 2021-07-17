54e00cdc45dda6d16a4ea75e7c4b0bdf0c617b14
Yuri Yasvoin <yasvoin@gmail.com>
2021-07-17 10:31:11 -0400

    Update ports

--- app.js
+++ app.js
@@ -135,11 +135,6 @@ app.get('/about', function (req, res) {
 	res.render('about');
 });
 
-let port = process.env.PORT;
-if (port == null || port == '') {
-	port = 3000;
-}
-
-app.listen(port, function () {
+app.listen(process.env.PORT || 3000, function () {
 	console.log(`Server has started on port ${port}`);
 });
